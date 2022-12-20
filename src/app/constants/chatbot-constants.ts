49eb2152811df76b7fe30f4a96f96ab4ebd029a8
SavithiriPalanivleu <savithiri2507@gmail.com>
2022-12-20 14:50:38 +0530

    Add files via upload

---
+++ src/app/constants/chatbot-constants.ts
@@ -0,0 +1,4 @@
+export class ChatBotContants {
+    public static LOGO_IMAGE = "https://staging1-uploads.insent.ai/insentstaging1/logo-insentstaging1-1649232340484?1649232340561";
+    public static PUSHER_KEY ="cace7e82a44adb737fef";
+}
---
+++ src/app/model/user-info.model.ts
@@ -0,0 +1,26 @@
+export interface UserInfo {
+    channelId: string;
+    popupMessage: PopupMessage;
+    user: User;
+}
+
+export interface PopupMessage {
+    message: string;
+} 
+
+export interface User {
+    id: string;
+}
+
+export interface Message {
+    text: string;
+    id: number;
+    type: string;
+    sender: boolean;
+    isEditorActive: boolean;
+    label: string;
+}
+export interface UserMessage {
+    text: string;
+    user: string;
+}
